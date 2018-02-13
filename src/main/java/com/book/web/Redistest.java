510867ac5b3fe761e0a95f8c091a70e3aa81c2df
yuzongtao <yuzongtao@users.noreply.github.com>
2018-02-13 10:42:40 +0800

    修改redis测试类

--- src/main/java/com/book/web/Redistest.java
+++ src/main/java/com/book/web/Redistest.java
@@ -19,7 +19,7 @@ public class Redistest {
         //设置 redis 字符串数据
         jedis.set("andy", "nice && handsome");
         // 获取存储的数据并输出
-        System.out.println("Stored string in redis:: "+ jedis.get("andy"));
+        System.out.println("Stored string in redis: "+ jedis.get("andy"));
 
         //存储数据到列表中
         jedis.lpush("tutorial-list", "Redis");
@@ -28,13 +28,13 @@ public class Redistest {
         // 获取存储的数据并输出
         List<String> list = jedis.lrange("C", 0 ,5);
         for(int i=0; i<list.size(); i++) {
-            System.out.println("Stored string in redis:: "+list.get(i));
+            System.out.println("Stored string in redis: "+list.get(i));
         }
 
         // 获取数据并输出
         Set<String> set = jedis.keys("*");
         for(int i=0; i<set.size(); i++) {
-            System.out.println("List of stored keys:: "+ set.toString());
+            System.out.println("List of stored keys:"+ set.toString());
         }
     }
 }
